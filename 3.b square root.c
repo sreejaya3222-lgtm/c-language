6577d79339e8bb4c102027452674d905c9d13869
M. JAYA SRI <sreejaya3222@gmail.com>
2025-11-28 19:13:02 +0530

    Add files via upload

---
+++ 3.b square root.c
@@ -0,0 +1,10 @@
+// square root
+#include<stdio.h>
+main()
+{
+	int n,root;
+	printf("enter a number");
+	scanf("%d",n);
+	square root=(sqrt)(num);
+	printf(" the square root is %d is %d",x,root)
+}
