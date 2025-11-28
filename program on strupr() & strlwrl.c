1f2800194dad7141a468fd0c59c771d23836cf77
M. JAYA SRI <sreejaya3222@gmail.com>
2025-11-28 19:25:06 +0530

    Add files via upload

---
+++ program on strupr() & strlwrl.c
@@ -0,0 +1,10 @@
+// program on strupr()&strlwrl
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char s1[20]="aditya";
+	char s2[20]="UNIVERSITY";
+	printf("the aditya in the upper case is",strupr(s1));
+	printf("the UNIVERSITY in lower case is",strlwr(s2));
+}
