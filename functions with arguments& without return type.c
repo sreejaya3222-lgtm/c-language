b4890a9f5417c399b34394b3072b4203f554f9d3
M. JAYA SRI <sreejaya3222@gmail.com>
2025-11-28 19:17:13 +0530

    Add files via upload

---
+++ functions with arguments& without return type.c
@@ -0,0 +1,16 @@
+//functions with arjuments&without return type
+#include<stdio.h>
+void addition(int,int);
+main()
+{
+	int a,b;
+	printf("enter a&b values");
+	scanf("%d%d",&a,&b);
+	addition(a,b);
+}
+void addition (int p,int q)
+{
+	int result;
+	result=p+q;
+	printf("the result is %d",result);
+}
