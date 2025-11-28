486d1339cc0742033d19bdab7c65d02e6b585f66
M. JAYA SRI <sreejaya3222@gmail.com>
2025-11-28 19:15:31 +0530

    Add files via upload

---
+++ 5.leap year are not.c
@@ -0,0 +1,13 @@
+// leap year are not
+#include <stdio.h>
+main()
+{
+	int year;
+	printf("enter a year");
+	scanf("%d",&year);
+	if(year%4==0&&(year%100!=0 ||year%400==0))
+	printf("leap year");
+	else
+	printf("not a leap year");
+	
+}
