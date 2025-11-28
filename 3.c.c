1cf1fceffb7151487f61e7ed8b781d77f835a09d
M. JAYA SRI <sreejaya3222@gmail.com>
2025-11-28 18:37:08 +0530

    Add files via upload

---
+++ 3.c.c
@@ -0,0 +1,27 @@
+//find the area of circle,square,rectangle and triangle
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int r,s,l,b,h;
+	float(area,sarea,rarea,tarea);
+	pi=3.14
+	printf(" enter the radius:");
+	scanf("%d",&r);
+	printf("enter the slide:");
+	scanf("%d",&s);
+	printf("enter the length and breath:");
+	scanf("%d%d",&l,&b);
+	printf("enter the base and height:");
+	scanf("%d%d",&ba,&h);
+	c area=pi*r*r;
+	printf("the area of circle is % f\n",c area);
+	s area=s*s;
+	printf("the area of square is % f\n",s area);
+	r area=l*b;
+	printf("the area of rectangle is % f\n", r area);
+	t area=1/2.0*ba*h;
+	printf("the area of triangle is % f\n",t area);
+	
+	
+}
