b3bbce7b5c850750972f799abeb2d37bd4406390
M. JAYA SRI <sreejaya3222@gmail.com>
2025-11-28 19:23:00 +0530

    Add files via upload

---
+++ program on strcat().c
@@ -0,0 +1,11 @@
+// program on strcat()
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char s1[]="Aditya";
+	char s2[20]="university";
+	strcat(s1,s2);
+	printf("the concatenated string is %s",s1);
+}
+
