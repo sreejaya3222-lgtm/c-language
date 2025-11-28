ff1ceedbcc7289ab2df84f768bffcaf0bcf2c6c9
M. JAYA SRI <sreejaya3222@gmail.com>
2025-11-28 19:16:19 +0530

    Add files via upload

---
+++ find the length of string.c
@@ -0,0 +1,11 @@
+#include<string.h>
+#include<stdio.h>
+ main()
+ {
+ 	char s[10];
+ 	int l;
+ 	printf("enter string name");
+ 	scanf("%s",s);
+ 	l=strlen(s);
+ 	printf(" length of the string=%d",l);
+ }
